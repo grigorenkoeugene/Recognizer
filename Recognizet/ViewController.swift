ae0dface7c30e253afcc5fa5f4762853cbe385e9
Jenya Grigorenko <7327917@gmail.com>
2022-01-26 21:58:32 +0300

    Initial Commit

---
+++ Recognizet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recognizet
+//
+//  Created by Евгений Григоренко on 26.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
